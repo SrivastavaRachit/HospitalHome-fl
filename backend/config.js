bd38c3577aa8c4cd675a30152c8a6751a9e3b687
SrivastavaRachit <rachitsrivastava@gmail.com>
2024-10-01 23:13:33 +0530

    pop

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const api_config = {
     port: 5000,
-    db_url: "mongodb+srv://Rachit:rachit6518619@cluster0.yfa2c.mongodb.net/homehospital-fl?retryWrites=true&w=majority",
+    db_url: "mongodb+srv://Rachit:rs6518619@cluster0.yfa2c.mongodb.net/homehospital-fl",
     stripe_sk: "pk_test_51KtvTTSH5orWLNFB4WzOZb0MErvaHYmfowZJNoATpEoBdwe4XeWJC3j9RhuCbwLmbrZSGM1USh5kMoeWMrMGkl5Y00yBvLrBGS",
     // email: 'rachitsrivastava@gmail.com',
     // app_password: 'lcqfbskappyzidhy'
